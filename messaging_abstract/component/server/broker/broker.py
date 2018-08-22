2ad28fb86186cb92f429424b82dfa63c7de27fcb
Fernando Giorgetti <fgiorget@redhat.com>
2018-08-22 17:23:55 -0300

    UPD: Adjusted router and broker constructors

--- messaging_abstract/component/server/broker/broker.py
+++ messaging_abstract/component/server/broker/broker.py
@@ -1,5 +1,4 @@
-from executor import Executor
-
+from iqa_common.executor import Executor
 from messaging_abstract.component.server.server import Server
 from messaging_abstract.component.server.service import Service
 from messaging_abstract.node.node import Node
@@ -12,6 +11,10 @@ class Broker(Server):
     """
     supported_protocols = []
 
-    def __init__(self, name: str, node: Node, executor: Executor, service: Service):
+    def __init__(self, name: str, node: Node, executor: Executor, service: Service,
+                 broker_name: str=None, config: str=None, web_port=8161):
         super(Broker, self).__init__(name, node, executor, service)
         self.queues = Queues()
+        self.broker_name = broker_name
+        self.config = config
+        self.web_port = web_port
