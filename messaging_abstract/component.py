442c1f6f8668369b5b033621d8a957e0c591aab8
Fernando Giorgetti <fgiorget@redhat.com>
2018-08-22 17:23:55 -0300

    UPD: Adjusted router and broker constructors

--- messaging_abstract/component.py
+++
@@ -1,5 +0,0 @@
-
-
-class Component(object):
-    def __init__(self, node: Node, executor: Executor, name: str=None):
-        self.name = name
--- messaging_abstract/component/server/router/router.py
+++ messaging_abstract/component/server/router/router.py
@@ -1,4 +1,4 @@
-from executor import Executor
+from iqa_common.executor import Executor
 
 from messaging_abstract.component.server.server import Server
 from messaging_abstract.component.server.service import Service
@@ -10,5 +10,8 @@ class Router(Server):
     Abstract messaging Router
     """
 
-    def __init__(self, name: str, node: Node, executor: Executor, service: Service):
+    def __init__(self, name: str, node: Node, executor: Executor, service: Service,
+                 port=5672, config=None):
         super(Router, self).__init__(name, node, executor, service)
+        self.port = port
+        self.config = config
