a919d120dde60c273e15d4ae149ea952e2062138
Fernando Giorgetti <fgiorget@redhat.com>
2018-09-17 19:25:49 -0300

    UPD: Enhanced clients so they can be initialized from inventory

--- messaging_abstract/component/client/client.py
+++ messaging_abstract/component/client/client.py
@@ -6,7 +6,7 @@ from iqa_common.executor import Executor, Execution
 from messaging_abstract.component.client.command.client_command import ClientCommand
 from messaging_abstract.component.component import Component
 from messaging_abstract.node.node import Node
-
+from inspect import signature
 
 class Client(Component):
     """
@@ -17,7 +17,7 @@ class Client(Component):
     supported_protocols = []
     version = ''
 
-    def __init__(self, name: str, node: Node, executor: Executor):
+    def __init__(self, name: str, node: Node, executor: Executor, **kwargs):
         super(Client, self).__init__(name, node, executor)
         self.logs = None  # @TODO
 
@@ -36,13 +36,43 @@ class Client(Component):
 
 class ClientExternal(Client):
 
-    TIMEOUT = 30
+    # Default is run forever
+    # As mixing --timeout with --count is causing issues
+    TIMEOUT = 90
 
-    def __init__(self, name: str, node: Node, executor: Executor):
-        super(ClientExternal, self).__init__(name, node, executor)
+    def __init__(self, name: str, node: Node, executor: Executor, **kwargs):
+        super(ClientExternal, self).__init__(name, node, executor, **kwargs)
         self.execution = None  # type: Execution
         self._command = None  # type: ClientCommand
+        self._url = None  # type: str
         self.reset_command()
+
+        # initializing client from kwargs
+        for func in [self.set_url, self.set_auth_mechs, self.set_ssl_auth]:
+            self.call_if_all_arguments_in_kwargs(func, **kwargs)
+
+    @staticmethod
+    def call_if_all_arguments_in_kwargs(func, **kwargs):
+        """
+        Call the given function if all declared arguments exist in
+        the kwargs dictionary. In example, if passed function is set_ssl_auth,
+        it will be called if kwargs contains the following keys:
+        pem_file, key_file, keystore, keystore_pass and keystore_alias.
+        :param func:
+        :param extra_args:
+        :return:
+        """
+        # kwargs not informed
+        if not kwargs:
+            return
+
+        # Not all function arguments needed are available in kwargs
+        if not all([k in list(kwargs.keys()) for k in list(signature(func).parameters.keys())]):
+            return
+
+        # Calling function if all args present in kwargs
+        arg_dict = {k: v for k, v in kwargs.items() if k in list(signature(func).parameters.keys())}
+        func(**arg_dict)
 
     @property
     def command(self) -> ClientCommand:
@@ -52,12 +82,18 @@ class ClientExternal(Client):
         self._command = self._new_command(stdout=True, timeout=ClientExternal.TIMEOUT,
                                           daemon=True)  # type: ClientCommand
 
+    def get_url(self):
+        return self._url
+
+    def set_url(self, url: str):
+        self._set_url(url)
+
     def _new_command(self, stdout: bool=False, stderr: bool=False,
                 daemon: bool=False, timeout: int=0,
                 encoding: str="utf-8") -> ClientCommand:
         raise NotImplementedError
 
-    def set_url(self, url: str):
+    def _set_url(self, url: str):
         raise NotImplementedError
 
     def set_auth_mechs(self, mechs: str):
