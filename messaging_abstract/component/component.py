a43141a0b0e5996bd69e056fcd32f8a250cf10bf
Fernando Giorgetti <fgiorget@redhat.com>
2018-09-18 17:24:53 -0300

    UPD: Adjusted router abstract to hold credentials for authentication

--- messaging_abstract/component/component.py
+++ messaging_abstract/component/component.py
@@ -1,6 +1,6 @@
 from iqa_common.executor import Executor, Execution, Command
-
 from messaging_abstract.node.node import Node
+from inspect import signature
 
 
 class Component(object):
@@ -16,3 +16,26 @@ class Component(object):
     @property
     def implementation(self):
         raise NotImplementedError
+
+    @staticmethod
+    def call_if_all_arguments_in_kwargs(func, **kwargs):
+        """
+        Call the given function if all declared arguments exist in
+        the kwargs dictionary. In example, if passed function is set_ssl_auth,
+        it will be called if kwargs contains the following keys:
+        pem_file, key_file, keystore, keystore_pass and keystore_alias.
+        :param func:
+        :param extra_args:
+        :return:
+        """
+        # kwargs not informed
+        if not kwargs:
+            return
+
+        # Not all function arguments needed are available in kwargs
+        if not all([k in list(kwargs.keys()) for k in list(signature(func).parameters.keys())]):
+            return
+
+        # Calling function if all args present in kwargs
+        arg_dict = {k: v for k, v in kwargs.items() if k in list(signature(func).parameters.keys())}
+        func(**arg_dict)
--- messaging_abstract/component/server/router/router.py
+++ messaging_abstract/component/server/router/router.py
@@ -15,3 +15,28 @@ class Router(Server):
         super(Router, self).__init__(name, node, executor, service)
         self.port = kwargs.get('router_port', port)
         self.config = kwargs.get('router_config', config)
+        self.user = None
+        self.password = None
+        self.pem_file = None
+        self.key_file = None
+        self.key_password = None
+
+        # initializing client from kwargs
+        for func in [self.set_credentials, self.set_ssl_auth]:
+            self.call_if_all_arguments_in_kwargs(func, **kwargs)
+
+    def set_credentials(self, user: str=None, password: str=None):
+        self.user = user
+        self.password = password
+
+    def set_ssl_auth(self, pem_file: str=None, key_file: str=None, key_password: str=None):
+        self.pem_file = pem_file
+        self.key_file = key_file
+        # Set to None when empty as well
+        self.key_password = None if not key_password else key_password
+
+    def has_credentials(self) -> bool:
+        return self.user and self.password
+
+    def has_ssl_keys(self):
+        return self.pem_file and self.key_file
