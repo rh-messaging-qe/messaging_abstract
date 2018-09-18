ba080b6c4d1bfc5d848a9e070780c69ca91bd239
Fernando Giorgetti <fgiorget@redhat.com>
2018-09-18 17:24:53 -0300

    UPD: Adjusted router abstract to hold credentials for authentication

--- messaging_abstract/component/client/client.py
+++ messaging_abstract/component/client/client.py
@@ -6,7 +6,7 @@ from iqa_common.executor import Executor, Execution
 from messaging_abstract.component.client.command.client_command import ClientCommand
 from messaging_abstract.component.component import Component
 from messaging_abstract.node.node import Node
-from inspect import signature
+
 
 class Client(Component):
     """
@@ -50,29 +50,6 @@ class ClientExternal(Client):
         # initializing client from kwargs
         for func in [self.set_url, self.set_auth_mechs, self.set_ssl_auth]:
             self.call_if_all_arguments_in_kwargs(func, **kwargs)
-
-    @staticmethod
-    def call_if_all_arguments_in_kwargs(func, **kwargs):
-        """
-        Call the given function if all declared arguments exist in
-        the kwargs dictionary. In example, if passed function is set_ssl_auth,
-        it will be called if kwargs contains the following keys:
-        pem_file, key_file, keystore, keystore_pass and keystore_alias.
-        :param func:
-        :param extra_args:
-        :return:
-        """
-        # kwargs not informed
-        if not kwargs:
-            return
-
-        # Not all function arguments needed are available in kwargs
-        if not all([k in list(kwargs.keys()) for k in list(signature(func).parameters.keys())]):
-            return
-
-        # Calling function if all args present in kwargs
-        arg_dict = {k: v for k, v in kwargs.items() if k in list(signature(func).parameters.keys())}
-        func(**arg_dict)
 
     @property
     def command(self) -> ClientCommand:
