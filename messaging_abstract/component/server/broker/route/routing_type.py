a54ad573e19b44f82a42b8b0278b76887cff2784
Fernando Giorgetti <fgiorget@redhat.com>
2018-11-30 17:24:16 -0200

    UPD(issue-5): Another change needed for RoutingType

--- messaging_abstract/component/server/broker/route/routing_type.py
+++ messaging_abstract/component/server/broker/route/routing_type.py
@@ -8,3 +8,15 @@ class RoutingType(Enum):
     ANYCAST = auto()
     MULTICAST = auto()
     BOTH = auto()
+
+    @staticmethod
+    def from_value(value: str):
+        if not value:
+            return RoutingType.ANYCAST
+
+        if value == 'ANYCAST':
+            return RoutingType.ANYCAST
+        elif value == 'MULTICAST':
+            return RoutingType.MULTICAST
+        else:
+            return RoutingType.BOTH
