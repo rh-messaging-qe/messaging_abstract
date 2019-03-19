8ca68c893b1314cda337468c543ea32fed3ae04a
Michal Tóth <mtoth@redhat.com>
2019-03-19 15:56:22 +0100

    NEW(ServiceFake)
    NEW(VirtualComponent) initial design for Vcomp
    
    NEW(BrokerCluster) Using VirtualComponent

--- messaging_abstract/component/server/__init__.py
+++ messaging_abstract/component/server/__init__.py
@@ -2,3 +2,4 @@ from .server import *
 from .router import *
 from .broker import *
 from .service import *
+from .service_fake import *
--- messaging_abstract/component/server/service_fake.py
+++ messaging_abstract/component/server/service_fake.py
@@ -1,6 +1,8 @@
+import abc
 from enum import Enum
 
 from iqa_common.executor import Execution, Executor
+from .service import Service
 
 
 class ServiceStatus(Enum):
@@ -10,7 +12,7 @@ class ServiceStatus(Enum):
     UNKNOWN = 'unknown'
 
 
-class Service(object):
+class ServiceFake(Service):
     """
     Represents a service used to control a Server component (Router or Broker).
     """
@@ -18,9 +20,11 @@ class Service(object):
     TIMEOUT = 30
 
     def __init__(self, name: str, executor: Executor):
+        super().__init__(name, executor)
         self.name = name
         self.executor = executor
 
+    @abc.abstractmethod
     def status(self) -> ServiceStatus:
         """
         Returns the service status
@@ -29,17 +33,22 @@ class Service(object):
         """
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def start(self, wait_for_messaging=False) -> Execution:
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def stop(self) -> Execution:
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def restart(self, wait_for_messaging=False) -> Execution:
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def enable(self) -> Execution:
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def disable(self) -> Execution:
         raise NotImplementedError()
